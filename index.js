f838d1780c1ad8753a6d892cbbc3926f6a189996
Kristina <kristin.petrivska@gmail.com>
2024-07-30 01:03:59 -0600

    add code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+"use strict";
+let ladder = {
+    step: 0,
+    up: function () {
+    this.step ++;
+    return ladder;
+    },
+    down: function () { 
+    this.step --;
+    return ladder;
+    },
+    showStep: function () {
+    console.log(this.step);
+    return ladder;
+    }
+  };
 
+   ladder.up().up().down().showStep()
